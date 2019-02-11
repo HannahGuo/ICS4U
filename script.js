2c90a769c2cd736120fc291585c12e6527b3cfbf
Hannah Guo <hguo2102@gmail.com>
2019-02-10 19:48:38 -0500

    Home finished, m1 pending

---
+++ script.js
@@ -0,0 +1,52 @@
+function loadM1(){
+    $("#main").load("pages/m1.html");
+};
+
+$(document).ready(function(){
+    $("#main").load("pages/home.html");
+});
+
+$("#home").click(function () {
+    $("#main").load("pages/home.html");
+    return false;
+});
+
+$("#m1").click(function () {
+    $("#main").load("pages/m1.html");
+    return false;
+});
+
+$("#m2").click(function () {
+    $("#main").html("<h1 class=\"titleFont newLine\">Greetings, programmer... </h1>");
+    return false;
+});
+
+$("#m3").click(function () {
+    $("#main").html("<h1 class=\"titleFont newLine\">Greetings, programmer... </h1>");
+    return false;
+});
+
+$("#m4").click(function () {
+    $("#main").html("<h1 class=\"titleFont newLine\">Greetings, programmer... </h1>");
+    return false;
+});
+
+$("#m5").click(function () {
+    $("#main").html("<h1 class=\"titleFont newLine\">Greetings, programmer... </h1>");
+    return false;
+});
+
+$("#m6").click(function () {
+    $("#main").html("<h1 class=\"titleFont newLine\">Greetings, programmer... </h1>");
+    return false;
+});
+
+$("#m7").click(function () {
+    $("#main").html("<h1 class=\"titleFont newLine\">Greetings, programmer... </h1>");
+    return false;
+});
+
+$("#m8").click(function () {
+    $("#main").load("home.html");
+    return false;
+});
